d6d76e3aba1a87ff0dea866639f3fdf3994d697c
Rohit Patel <rohit.patel@infibeam.net>
2019-10-07 21:48:43 +0530

    added express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require("express");
 
 const router = express.Router();
-// @route api/users
-// @desc test route
+const { check, validationResult } = require('express-validator');
+
+// @route POST api/users
+// @desc Register user
 // @access public
-router.get('/', (req, res) => res.send('Users route'));
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please enter a valid email').isEmail(),
+  check('password', 'Password should be of minimum 6 characters').isLength({ min: 6 })
+
+	],(req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+	res.send('Users route')
+});
 
 module.exports = router;
