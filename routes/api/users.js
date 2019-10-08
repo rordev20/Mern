9e55d48188256a586076e791ced72e4601ea9c66
Rohit Patel <rohit.patel@infibeam.net>
2019-10-08 10:32:25 +0530

    jwt token implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const bcrypt = require("bcryptjs");
 
@@ -50,8 +52,21 @@ router.post('/', [
 
     await user.save();
 
-    // Return jsonwebtoken
-    res.send('Users route')
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {expiresIn: 360000},
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      }
+    );
 
   } catch(err) {
     console.error(err.message);
