73dffa2cdc26b9b56faec6737f554bd8c5588c4b
Rohit Patel <rohit.patel@infibeam.net>
2019-10-10 01:44:58 +0530

    delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -223,5 +223,25 @@ router.put('/experience', [auth, [
 })
 
 
+// @route delet api/profile/experience/:exp_id
+// delete profile experience
+// @access Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+    // get remove index
+    const removeIndex = profile.experience.map(exp => exp.id).indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+
+
+})
+
 
 module.exports = router;
