b1cc6b51190776aa47aebb16b8542edaab8fd5cf
Rohit Patel <rohit.patel@infibeam.net>
2019-11-15 12:24:29 +0530

    load user and setauthtoken

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
 	REGISTER_SUCCESS,
-	REGISTER_FAIL
+	REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -14,6 +16,25 @@ export default function(state = initialState, action) {
   const {type, payload} = action;
 
   switch(type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      }
+    case AUTH_ERROR:
+      if (payload) {
+         localStorage.removeItem('token', payload.token);
+      }
+
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      }
+
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
